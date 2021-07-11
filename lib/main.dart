651d4225a3827505bd7fcd8b6049635fa1117328
ANKURJANA-1 <ankurjana905@gmail.com>
2021-07-11 17:18:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,323 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "hello world",
+      home:MyApp()
+      
+    )
+  );
+  
+  }
+
+ class MyApp extends StatelessWidget{
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+       appBar: AppBar(
+         title: Text(
+           "hello world"
+         ),
+       ),
+       body: Column(
+         crossAxisAlignment: CrossAxisAlignment.end,
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         children: [
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+               
+             ],
+           ),
+            Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+               
+             ],
+           ),
+            Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+               
+             ],
+           ),
+            Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+                Center(
+                 child: Container(
+                   width: 100,
+                   height: 100,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                       color: Colors.red,
+                       gradient: LinearGradient( colors: [
+                         Colors.red,
+                         Colors.green]),
+                      // shape: BoxShape.circle,
+                      borderRadius: BorderRadius.circular(10)   
+                   ),
+                   child: Text(
+                     "First App",
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.white,
+                     ),
+                   ),
+                 ),
+               ),
+               
+             ],
+           ),
+         ],
+         
+       ),
+      );
+    }
+
+ }
+  
